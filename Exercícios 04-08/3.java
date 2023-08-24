0de6a0b0dbf322c1d9dab56a648a9fa80eaf36ae
Khalil <khalil.bm@hotmail.com>
2023-08-23 23:22:55 -0300

    atividades 04/08

---
+++ Exercícios 04-08/3.java
@@ -0,0 +1,27 @@
+/*3) A importância de R$ 780.000,00 será dividida entre três ganhadores de um concurso.
+Sendo que da quantia total:
+● O primeiro ganhador recebera 46%;
+● O segundo recebera 32%;
+● O terceiro recebera o restante.
+Calcule e imprima a quantia ganha por cada um dos ganhadores */
+
+import java.util.Scanner;
+
+
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    double premio, primeiro, segundo, terceiro;
+
+    System.out.println("Digite o valor do prêmio: ");
+    premio = input.nextDouble();
+
+    primeiro = premio * 0.46;
+    segundo = premio * 0.32;
+    terceiro = premio - (primeiro + segundo);
+
+    System.out.println("O primeiro ganhador receberá: " + primeiro);
+    System.out.println("O segundo ganhador receberá: " + segundo);
+    System.out.println("O terceiro ganhador receberá: " + terceiro);
+    
+}
